09a8b6c8661c21d6f16acae695c6f389fcbc1ce4
DESKTOP-SQJLPB3\burak <burakozn@gmail.com>
2019-05-18 09:37:42 +0300

    Edit: Singleton

--- EduProfile/WebUI/Controllers/HomeController.cs
+++ EduProfile/WebUI/Controllers/HomeController.cs
@@ -12,13 +12,18 @@ namespace WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<User> ur;
+        public HomeController(IRepository<User> repository)
+        {
+            ur = repository;
+        }
         public IActionResult Index()
         {
             User usr = new User() { Birth = DateTime.Now, CreatedAt = DateTime.Now, Email = "burakozn@gmail.com", LastName = "OZEN", Name = "Ahmet", CreatedBy = "Burak", PictureURL = "asdas", Password = "124", UpdatedAt = DateTime.Now, UpdatedBy = ".can" };
 
             //var a = br.Add(usr);
             //a.Wait();
-            var test = UserRepository.Instance.Get();
+            var test = ur.Get();
             test.Wait();
             List<User> users = test.Result;
             return View();
