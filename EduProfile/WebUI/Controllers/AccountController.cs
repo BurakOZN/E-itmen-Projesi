1f5460d97a44ce1c617773c60b0e7f45e9115d93
DESKTOP-SQJLPB3\burak <burakozn@gmail.com>
2019-05-20 14:30:44 +0300

    Add: Login ekranı dosyaları

---
+++ EduProfile/WebUI/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL.Manager;
+using BLL.Repository;
+using Entity.DataModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IRepository<User> _userRep;
+        public AccountController(IRepository<User> br)
+        {
+            _userRep = br;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            UserManager um = new UserManager(_userRep);
+            User user= um.Find(x => x.Email == "burakozn@gmail.com" && x.Id== "5cdc21af4d3c8d15d83d6cca");
+            return await Task.Run(()=>View());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(User user)
+        {
+
+            return await Task.Run(() => View());
+        }
+
+    }
+}
